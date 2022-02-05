ed0031bf99a5aae686a7171d68ace7422e438011
kenyakween <96195907+kenyakween@users.noreply.github.com>
2022-02-05 11:47:23 -0600

    Changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
+    $(".navbar-nav a").click(function(){
+        debugger
+        $("body").animate({
+        scrollTop:$("#" + $(this).data('value')).offset().top
+    },1000)
+})
+
+
 })
