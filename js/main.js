b75e810583a745a28bfcfabcb0451ae4fcd962dd
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-02-08 18:03:43 +0000

    Fix about button
    Animate from html,body

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    $('.header').height($(window).height());
-    $(".navbar-nav a").click(function(){
-        debugger
-        $("body").animate({
-        scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
-})
-
-
+$(document).ready(function () {
+    $(".header").height($(window).height())
+    $(".navbar-nav a").click(function (e) {
+        e.preventDefault()
+        const distanceFromTopToAboutSection = $(
+            "#" + $(this).data("value")
+        ).offset().top
+        $("html,body").animate(
+            {
+                scrollTop: distanceFromTopToAboutSection,
+            },
+            1000
+        )
+    })
 })
