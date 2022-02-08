d36cae303f4afb9b816c0c8bc2e0b57dd78a5b53
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2022-02-08 18:05:51 +0000

    add comments to js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
+    // make header section as tall as the window
     $(".header").height($(window).height())
+
+    // when user clicks on one of the nav buttons, we scroll to the section with 
+    // an id equal to the button's data-value attribute.
     $(".navbar-nav a").click(function (e) {
         e.preventDefault()
         const distanceFromTopToAboutSection = $(
